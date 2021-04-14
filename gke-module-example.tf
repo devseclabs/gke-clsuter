3f1e21b3eb3c86650f543ec5f7590d010ca0aa09
alejandro <achdevops@gmail.com>
2021-04-14 16:30:54 -0300

    update

--- gke-module-example.tf
+++ gke-module-example.tf
@@ -2,6 +2,6 @@ module "nv-gke-cluster" {
   source                = "git::https://github.com/devseclabs/gke-cluster.git"
   
   cluster_name          = "nvlabs"
-  project_id            = "nv-demo"
+  project_id            = "my-project-id"
     
 }
